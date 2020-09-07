f9448376b29896854f8d4756b836dfab7e25ac85
reinabawag <reinabawag@gmail.com>
2020-09-07 16:54:34 +0800

    Updated Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HR Covid Tracker
                 </div>
 
                 <div class="links">
